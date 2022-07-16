fa20b8f2c3dc74be812609e59c77454997aa2727
ilyassstri <trikhaqiqi@gmail.com>
2022-07-16 10:53:21 +0700

    add validation, handling validation errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,38 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-    res.send('Hi there!');
+router.post(
+    '/api/users/signup', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email must be valid!'),
+        body('password')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Password must be between 4 and 20 characters')
+    ], 
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).send(errors.array())
+        }
+
+        // res.send('Hi there!');
+        const { email, password } = req.body;
+
+        console.log('Creating a user...');
+        
+        res.send({});
+
+        // if ( !email || typeof email !== 'string' ) {
+        //     res.status(400).send('Provide a valid email');
+        // }
+
+    // new User({ email, password  })
 });
 
 export { router as signupRouter };
