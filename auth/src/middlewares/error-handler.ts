5d0a4b7e3f71e5a8b593b85555946be918192cb8
ilyassstri <trikhaqiqi@gmail.com>
2022-07-18 21:26:15 +0700

    Converting, moving logic, verifying, final, define new custom error, asyn error

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+// import { RequestValidationError } from "../errors/request-validation-error";
+// import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
     err: Error,
@@ -8,14 +9,27 @@ export const errorHandler = (
     res: Response,
     next: NextFunction
     ) => {
-        if (err instanceof RequestValidationError) {
-            console.log("handling this error as a request validation error");
+        if (err instanceof CustomError) {
+            // console.log("handling this error as a request validation error");
+            
+            // const formattedErrors = err.errors.map(error => {
+            //     return { message: error.msg, field: error.param };
+            // });
+
+            // return res.status(400).send({ errors: formattedErrors });
+
+            return res.status(err.statusCode).send({ errors: err.serializeErrors() });
         }
-        if (err instanceof DatabaseConnectionError) {
-            console.log("handling this error as a db connection error");
-        }
+        // if (err instanceof DatabaseConnectionError) {
+            // console.log("handling this error as a db connection error");
+
+            // return res.status(500).send({ errors: [{ message: err.reason }] })
+
+            // return res.status(err.statusCode).send({ errors: [{ message: err.serializeErrors() }] })
+        // }
 
     res.status(400).send({
-        message: err.message,
+        // message: err.message,
+        errors: [{ message: 'Something went wrong' }]
     });
 };
