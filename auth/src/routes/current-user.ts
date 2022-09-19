ed1716ffbe0a979d5d7d590ce3a901ecb1dbde37
trikhaqiqi <trikhaqiqi@gmail.com>
2022-09-19 16:32:20 +0700

    add currentUser signout, middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,26 @@
 import express from 'express';
 
+import { currentUser } from '../middlewares/current-user'; 
+
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
+router.get('/api/users/currentuser',currentUser,  (req, res) => {
+    // res.send('Hi there!');
+    // if (!req.session?.jwt) {
+    //     return res.send({ currentUser: null });
+    // }
+
+    // try {
+    //     const payload = jwt.verify(
+    //         req.session.jwt, 
+    //         process.env.JWT_KEY!
+    //     );
+    //     res.send({ currentUser: payload });
+    // } catch (err) {
+    //     res.send({ currentUser: null });
+    // }
+
+    res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
